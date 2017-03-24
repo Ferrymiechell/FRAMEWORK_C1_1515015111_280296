85cc0397b87ae3f2783e7a56714c95509f43e2f3
ferrymiechell <ferrymiechel@gmail.com>
2017-03-24 14:40:44 +0700

    Postest Modul 4 View,Edit,Update,Delete,Tambah,Awal)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,14 +10,40 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/',function(){
+    return view('master');
+});
+
+Route::get('pengguna', 'PenggunaController@awal');
+Route::get('pengguna/lihat/{pengguna}','PenggunaController@lihat');
+Route::post('pengguna/simpan','PenggunaController@simpan');
+Route::get('pengguna/edit/{pengguna}','PenggunaController@edit');
+Route::post('pengguna/edit/{pengguna}','PenggunaController@update');
+Route::get('pengguna/hapus/{pengguna}','PenggunaController@hapus');
+Route::get('pengguna/tambah', 'PenggunaController@tambah');
+
+Route::get('matakuliah', 'MatakuliahController@awal');
+Route::get('matakuliah/lihat/{matakuliah}','MatakuliahController@lihat');
+Route::post('matakuliah/simpan','MatakuliahController@simpan');
+Route::get('matakuliah/edit/{matakuliah}','MatakuliahController@edit');
+Route::post('matakuliah/edit/{matakuliah}','MatakuliahController@update');
+Route::get('matakuliah/tambah', 'MatakuliahController@tambah');
+Route::get('matakuliah/hapus/{matakuliah}','MatakuliahController@hapus');
+
+Route::get('ruangan', 'RuanganController@awal');
+Route::get('ruangan/lihat/{ruangan}','RuanganController@lihat');
+Route::post('ruangan/simpan','RuanganController@simpan');
+Route::get('ruangan/edit/{ruangan}','RuanganController@edit');
+Route::get('ruangan/tambah', 'RuanganController@tambah');
+Route::post('ruangan/edit/{ruangan}','RuanganController@update');
+Route::get('ruangan/hapus/{ruangan}','RuanganController@hapus');
 
 Route::get('/hello', function () {
     return "Hello Ferry welcome";
 });
 
-Route::get('pengguna', 'PenggunaController@awal');
 
-Route::get('pengguna/tambah', 'PenggunaController@tambah');
+
 
 Route::get('dosen', 'DosenController@awal');
 
@@ -26,14 +52,6 @@ Route::get('dosen/tambah', 'DosenController@tambah');
 Route::get('mahasiswa', 'MahasiswaController@awal');
 
 Route::get('mahasiswa/tambah', 'MahasiswaController@tambah');
-
-Route::get('ruangan', 'RuanganController@awal');
-
-Route::get('ruangan/tambah', 'RuanganController@tambah');
-
-Route::get('matakuliah', 'MatakuliahController@awal');
-
-Route::get('matakuliah/tambah', 'MatakuliahController@tambah');
 
 Route::get('dosen_matakuliah', 'Dosen_MatakuliahController@awal');
 
