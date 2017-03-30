4fdbc33821d93273e9e996e6c8c82a21e0dd3a7f
ferrymiechell <ferrymiechel@gmail.com>
2017-03-30 21:27:42 +0800

    Postest Modul 5 Eloquent Relationship Selesai

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -38,6 +38,40 @@ Route::get('ruangan/tambah', 'RuanganController@tambah');
 Route::post('ruangan/edit/{ruangan}','RuanganController@update');
 Route::get('ruangan/hapus/{ruangan}','RuanganController@hapus');
 
+Route::get('mahasiswa', 'MahasiswaController@awal');
+Route::get('mahasiswa/lihat/{mahasiswa}','MahasiswaController@lihat');
+Route::post('mahasiswa/simpan','MahasiswaController@simpan');
+Route::get('mahasiswa/edit/{mahasiswa}','MahasiswaController@edit');
+Route::get('mahasiswa/tambah', 'MahasiswaController@tambah');
+Route::post('mahasiswa/edit/{mahasiswa}','MahasiswaController@update');
+Route::get('mahasiswa/hapus/{mahasiswa}','MahasiswaController@hapus');
+
+Route::get('dosen', 'DosenController@awal');
+Route::get('dosen/lihat/{dosen}','DosenController@lihat');
+Route::post('dosen/simpan','DosenController@simpan');
+Route::get('dosen/edit/{dosen}','DosenController@edit');
+Route::get('dosen/tambah', 'DosenController@tambah');
+Route::post('dosen/edit/{dosen}','DosenController@update');
+Route::get('dosen/hapus/{dosen}','DosenController@hapus');
+
+
+Route::get('dosen_matakuliah','Dosen_MatakuliahController@awal');
+Route::get('dosen_matakuliah/tambah','Dosen_MatakuliahController@tambah');
+Route::post('dosen_matakuliah/simpan','Dosen_matakuliahController@simpan');
+Route::get('dosen_matakuliah/edit/{dosen_matakuliah}','Dosen_MatakuliahController@edit');
+Route::post('dosen_matakuliah/edit/{dosen_matakuliah}','Dosen_MatakuliahController@update');
+Route::get('dosen_matakuliah/hapus/{dosen_matakuliah}','Dosen_MatakuliahController@hapus');
+Route::get('dosen_matakuliah/lihat/{dosen_matakuliah}','Dosen_MatakuliahController@lihat');
+
+
+Route::get('jadwal_matakuliah','Jadwal_MatakuliahController@awal');
+Route::get('jadwal_matakuliah/tambah','Jadwal_MatakuliahController@tambah');
+Route::post('jadwal_matakuliah/simpan','Jadwal_MatakuliahController@simpan');
+Route::get('jadwal_matakuliah/edit/{jadwal_matakuliah}','Jadwal_MatakuliahController@edit');
+Route::post('jadwal_matakuliah/edit/{jadwal_matakuliah}','Jadwal_MatakuliahController@update');
+Route::get('jadwal_matakuliah/hapus/{jadwal_matakuliah}','Jadwal_MatakuliahController@hapus');
+Route::get('jadwal_matakuliah/lihat/{jadwal_matakuliah}','Jadwal_MatakuliahController@lihat');
+
 Route::get('/hello', function () {
     return "Hello Ferry welcome";
 });
